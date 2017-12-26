dd5468b3e76919aac7844db7ac49130d3e3b4441
Daniel Richert <daniel.richert@berge-meer.de>
2017-12-26 10:20:07 +0100

    added frontpage splash area and service area

---
+++ assets/javascripts/typewriter_useage.js
@@ -0,0 +1,22 @@
+var pause_time = 1500;
+var splash_screen_text = document.getElementById('splashscreen-text');
+
+var typewriter = new Typewriter(splash_screen_text, {
+    loop: true
+});
+
+typewriter.typeString('WEBSEITEN')
+    .pauseFor(pause_time)
+    .deleteAll()
+    .typeString('LOGOS')
+    .pauseFor(pause_time)
+    .deleteAll()
+    .typeString('VISITENKARTEN')
+    .pauseFor(pause_time)
+    .deleteAll()
+    .typeString('SCHULUNGEN')
+    .pauseFor(pause_time)
+    .deleteAll()
+    .typeString('BERATUNG')
+    .pauseFor(pause_time)
+    .start();
