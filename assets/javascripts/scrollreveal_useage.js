95b0de79aa76b21c2f4e56f1e1b97e2e01c1693c
Daniel Richert <daniel.richert@berge-meer.de>
2018-01-20 13:51:22 +0100

    updated jekyll and some template work

--- assets/javascripts/scrollreveal_useage.js
+++ assets/javascripts/scrollreveal_useage.js
@@ -2,6 +2,6 @@
 window.sr = ScrollReveal({ reset: false });
 
 // Customizing a reveal set
-sr.reveal('.slide-in-left', { duration: 1000, origin: 'left', delay: 200, opacity: 0, distance: '50px' });
-sr.reveal('.slide-in-right', { duration: 1000, origin: 'right', delay: 200, opacity: 0, distance: '50px' });
-sr.reveal('.fade-in', { duration: 1000, origin: 'right', delay: 200, opacity: 0, distance: '0px' });
+sr.reveal('.slide-in-left', { duration: 1000, origin: 'left', delay: 0, opacity: 0, distance: '50px' });
+sr.reveal('.slide-in-right', { duration: 1000, origin: 'right', delay: 0, opacity: 0, distance: '50px' });
+sr.reveal('.fade-in', { duration: 1000, origin: 'right', delay: 0, opacity: 0, distance: '0px' });
--- assets/javascripts/typewriter_useage.js
+++ assets/javascripts/typewriter_useage.js
@@ -1,22 +1,26 @@
-var pause_time = 1500;
-var splash_screen_text = document.querySelector('.splashscreen-text');
+$(document).ready(function() {
+    var pause_time = 1500;
+    var splash_screen_text = document.querySelector('#home .typewriter-text');
 
-var typewriter = new Typewriter(splash_screen_text, {
-    loop: true
+    if(splash_screen_text != undefined) {
+        var typewriter = new Typewriter(splash_screen_text, {
+            loop: true
+        });
+
+        typewriter.typeString('WEBSEITEN')
+            .pauseFor(pause_time)
+            .deleteAll()
+            .typeString('LOGOS')
+            .pauseFor(pause_time)
+            .deleteAll()
+            .typeString('VISITENKARTEN')
+            .pauseFor(pause_time)
+            .deleteAll()
+            .typeString('SCHULUNGEN')
+            .pauseFor(pause_time)
+            .deleteAll()
+            .typeString('BERATUNG')
+            .pauseFor(pause_time)
+            .start();
+    }
 });
-
-typewriter.typeString('WEBSEITEN')
-    .pauseFor(pause_time)
-    .deleteAll()
-    .typeString('LOGOS')
-    .pauseFor(pause_time)
-    .deleteAll()
-    .typeString('VISITENKARTEN')
-    .pauseFor(pause_time)
-    .deleteAll()
-    .typeString('SCHULUNGEN')
-    .pauseFor(pause_time)
-    .deleteAll()
-    .typeString('BERATUNG')
-    .pauseFor(pause_time)
-    .start();
